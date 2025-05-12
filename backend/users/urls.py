a244ce27987781fefbf26916d9ceb75dae37f61a
yh13431 <thiayihong@gmail.com>
2025-05-12 11:33:57 +0800

    Added jwt to auth endpoints

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_user, name='register_user'),
-    path('login/', views.login_user, name='login_user'),
-    path('update/', views.update_user, name='update_user'),
-    path('delete/', views.delete_user, name='delete_user'),
+    path('register', views.register_user, name='register_user'),
+    path('login', views.login_user, name='login_user'),
+    path('update', views.update_user, name='update_user'),
+    path('delete', views.delete_user, name='delete_user'),
     path('', views.get_all_users, name='get_all_users'),
-    path('<str:username>/', views.get_user, name='get_user'),
+    path('<str:username>', views.get_user, name='get_user'),
 ]
